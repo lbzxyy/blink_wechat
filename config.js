40c8fd7553003532ca19298416eb3fafc0dc9e8f
leepoo <leepoo2008@163.com>
2019-04-28 21:25:51 +0800

    封装http请求

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    api_base_url: "https://www.easy-mock.com/mock/5cc1d7a7aa4f7f6ebe5d2704/leepoo/"
+}
+
+export {
+    config
+}
---
+++ util/http.js
@@ -0,0 +1,74 @@
+import { config } from '../config.js'
+const tips = {
+    1: '抱歉，出现了一个错误',
+    1005: '1005错误'
+}
+class HTTP {
+    request(params){
+        if(!params.method) {
+            params.method = "GET"
+        }
+        wx.request({
+            url: config.api_base_url + params.url,
+            method: params.method,
+            data: params.data,
+            header: {
+                'content-type': 'application/json'
+            },
+            success: (res) => {
+                
+                // 处理状态情况
+                let code = res.statusCode.toString()
+                if(code.startsWith('2')){ // 状态码是2开头的 说明请求成功
+                    params.success(res)
+                }else{
+                    wx.showToast({
+                        title: '错误',
+                        icon: 'none',
+                        image: '',
+                        duration: 1500,
+                        mask: false,
+                        success: (result)=>{
+                            
+                        },
+                        fail: ()=>{},
+                        complete: ()=>{}
+                    });
+                }
+            },
+            fail:(error) => {
+                wx.showToast({
+                    title: '错误',
+                    icon: 'none',
+                    image: '',
+                    duration: 1500,
+                    mask: false,
+                    success: (result)=>{
+                        
+                    },
+                    fail: ()=>{},
+                    complete: ()=>{}
+                });
+            }
+
+        })
+    }
+    _show_error(erro_code){
+        wx.showToast({
+            title: tips[erro_code],
+            icon: 'none',
+            image: '',
+            duration: 1500,
+            mask: false,
+            success: (result)=>{
+                
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+        })
+    }
+}
+
+export {
+    HTTP
+}
