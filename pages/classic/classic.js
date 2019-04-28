c1f4f25f44c26683dbdbcf3598d26f71cd433dcd
leepoo <leepoo2008@163.com>
2019-04-28 22:50:29 +0800

    movie组件的编写

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,25 +1,25 @@
 // pages/classic/classic.js
-import { HTTP } from '../../util/http.js'
-let http = new HTTP()
+import { ClassicModel } from '../../models/classic.js'
+let classic = new ClassicModel()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      classic: null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    http.request({
-      url: 'classic/latest',
-      success: (res) => {
-        console.log(res);
-        
-      }
+    classic.getLatest(res=>{
+      console.log(res);
+
+        this.setData({
+          classic: res
+        })
     })
   },
 
