ed1082c2797830839f057cbadbb245f09b849ee4
leepoo <leepoo2008@163.com>
2019-04-28 21:25:28 +0800

    封装http请求

--- components/like/index.js
+++ components/like/index.js
@@ -4,14 +4,24 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    like: {
+      type: Boolean,
+      value: false,
+      observer: function() {
 
+      }
+    },
+    count: {
+      type: Number
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
   },
 
   /**
@@ -20,6 +30,13 @@ Component({
   methods: {
     onLike:function(event) {
       console.log(event)
+      let like = this.properties.like;
+      let count = this.properties.count;
+      count= like?count-1:count+1;
+      this.setData({
+        count: count,
+        like: !like
+      })
     }
   }
 })
--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,4 +1,6 @@
 // pages/classic/classic.js
+import { HTTP } from '../../util/http.js'
+let http = new HTTP()
 Page({
 
   /**
@@ -12,7 +14,13 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    http.request({
+      url: 'classic/latest',
+      success: (res) => {
+        console.log(res);
+        
+      }
+    })
   },
 
   /**
